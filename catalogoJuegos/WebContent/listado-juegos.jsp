fa0633ebccefd52f59552ee8a8d777e5d93fc2f9
Alvaro <javaee@javaee-VirtualBox>
2020-06-08 12:06:45 +0200

    Se agrega cabecera y pie

--- catalogoJuegos/WebContent/listado-juegos.jsp
+++ catalogoJuegos/WebContent/listado-juegos.jsp
@@ -1,37 +1,30 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>  
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>listado de juegos</title>
-</head>
-<body>
+<c:import url="includes/cabecera.jsp">
+
+</c:import>
+<div class="container">
 	<h1>Listado de juegos</h1>
 	
 
-	    
+	 <table class="table"> 
+		  <thead class="thead-dark">
+		    <tr>
+		      <th>Id</th>
+		      <th>Nombre</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		    	<c:forEach items="${juegos }"  var="juego">
+			    	<tr>
+			    		<td>${juego.id}</td>
+			    		<td>${juego.nombre}</td>
+			    	</tr>
+			    </c:forEach>
+		        
+		  </tbody>
+		
+	</table> 
+</div>
 
-	
- <table>
-  <thead>
-    <tr>
-      <th>Id</th>
-      <th>Nombre</th>
-    </tr>
-  </thead>
-  <tbody>
-    	<c:forEach items="${juegos }"  var="juego">
-	    	<tr>
-	    		<td>${juego.id}</td>
-	    		<td>${juego.nombre}</td>
-	    	</tr>
-	    </c:forEach>
-        
-  </tbody>
-
-</table> 
-
-</body>
-</html>
+<c:import url="includes/pie.jsp">
+</c:import>
---
+++ catalogoJuegos/script-db-catalogo-juegos.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: catalogoJuegos
+-- ------------------------------------------------------
+-- Server version	5.7.30-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `catalogoJuegos`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `catalogoJuegos` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+
+USE `catalogoJuegos`;
+
+--
+-- Table structure for table `juegos`
+--
+
+DROP TABLE IF EXISTS `juegos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `juegos` (
+  `nombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `juegos_UN` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Listado de juegos';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `juegos`
+--
+
+LOCK TABLES `juegos` WRITE;
+/*!40000 ALTER TABLE `juegos` DISABLE KEYS */;
+INSERT INTO `juegos` VALUES ('FarCry 4',10),('FarCry primal',11),('Forza 5',12),('GTAV',8),('The Witcher 3',9);
+/*!40000 ALTER TABLE `juegos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-08 10:06:01
