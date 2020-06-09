0e69baa8831c643960af523869e4790192111fb1
javaee <javaee@javaee-VirtualBox>
2020-06-09 10:48:03 +0200

    Se agregan mensajes de alerta

--- catalogoJuegos/src/main/java/catalogoJuegos/utilidades/Constantes.java
+++ catalogoJuegos/src/main/java/catalogoJuegos/utilidades/Constantes.java
@@ -6,4 +6,23 @@ public class Constantes {
 	public static final String INICIO_JUEGOS = "Inicio | juegos";
 	public static final String JUEGOS = "juegos";
 	public static final String LISTADO_JUEGOS_JSP = "listado-juegos.jsp";
+	
+	
+	
+	// mensajes alerta
+	
+	public static final String DANGER = "danger";
+	public static final String INSERT_CORRECTO = "Insercion correcta";
+	public static final String NOMBRE_LONGITUD_INCORRECTA = "La longitud es incorrecta";
+	public static final String NOMBRE_YA_EXISTE = "El nombre ya existe";
+	public static final String SUCCESS = "success";
+	public static final String WARNING = "warning";
+	
+	
+	
+	//constantes numericas
+	
+	public static final int TRES=3;
+	public static final int CIEN=100;
+
 }
