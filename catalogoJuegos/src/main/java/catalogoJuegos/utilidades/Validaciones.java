f5c7361083a6ecf4eed9f2b4b7b3848a9f89c46c
javaee <javaee@javaee-VirtualBox>
2020-06-09 10:48:38 +0200

    Se agrega validacion longitud cadena

---
+++ catalogoJuegos/src/main/java/catalogoJuegos/utilidades/Validaciones.java
@@ -0,0 +1,24 @@
+package catalogoJuegos.utilidades;
+
+public class Validaciones {
+
+	
+	/**
+	 * Método que comprueba que la longitud de una cadena
+	 * 		esta entre 2 valores
+	 * @param cadena cadena a comprobar
+	 * @param min tamaño minimo
+	 * @param max tamaño maximo
+	 * @return boolean true si cadena esta entre min y max(ambos incluidos)
+	 */
+	public static boolean longitud(String cadena, int min, int max) {
+
+		boolean correcto = false;
+
+		if (cadena.length() >= min && cadena.length() <= 100) {
+			correcto=true;
+		}
+
+		return correcto;
+	}
+}
