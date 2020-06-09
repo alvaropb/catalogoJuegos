aa1eb8a580c8c80febfaa4f3443dca373a29f077
javaee <javaee@javaee-VirtualBox>
2020-06-09 10:48:17 +0200

    Se agregan alertas

---
+++ catalogoJuegos/src/main/java/catalogoJuegos/utilidades/Alerta.java
@@ -0,0 +1,32 @@
+package catalogoJuegos.utilidades;
+
+public class Alerta {
+
+	private String mensaje;
+	private String tipo;
+	
+		
+	public Alerta(String mensaje, String tipo) {
+		this();
+		this.mensaje = mensaje;
+		this.tipo = tipo;
+	}
+	public Alerta() {
+		super();
+		this.mensaje="";
+		this.tipo="";
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
