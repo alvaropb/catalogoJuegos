07c6cbe26c7e8910896e04106903c8ba59488d11
Alvaro <javaee@javaee-VirtualBox>
2020-06-10 13:54:02 +0200

    Se añade: getById, update y delete

--- catalogoJuegos/src/main/java/catalogoJuegos/modelo/ICrudable.java
+++ catalogoJuegos/src/main/java/catalogoJuegos/modelo/ICrudable.java
@@ -27,5 +27,29 @@ public interface ICrudable<T> {
 	  */
 	public T getByName(T t)throws Exception;
 	
+	
+	/**
+	 * Método que busca y retorna un <T> 
+	 * @param t
+	 * @return <T>
+	 * @throws Exception
+	 */
+	public T getById(T t) throws Exception;
+	
+	/**
+	 * Método que actualiza un <T>
+	 * @param <T>
+	 * @return <T>
+	 * @throws Exception
+	 */
+	public T update(T t)throws Exception;
+	
+	/**
+	 * Método que borra un <T>
+	 * @param <T>
+	 * @return <T>
+	 * @throws Exception
+	 */
+	public T delete(T t)throws Exception;
 
 }
