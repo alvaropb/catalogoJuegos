0eed9e00b6970381c2ca87cfc6943a7096df1338
Alvaro <javaee@javaee-VirtualBox>
2020-06-10 13:46:59 +0200

    Se ordenan, quitar espacios y saltos de linea,Agregado ctes

--- catalogoJuegos/src/main/java/catalogoJuegos/utilidades/Constantes.java
+++ catalogoJuegos/src/main/java/catalogoJuegos/utilidades/Constantes.java
@@ -3,32 +3,34 @@ package catalogoJuegos.utilidades;
 public class Constantes {
 
 	// atributos
-	
+
 	public static final String PAGINA = "pagina";
 	public static final String INICIO_JUEGOS = "Inicio | juegos";
 	public static final String JUEGOS = "juegos";
-	
+
 	// paginas
-	
-	public static final String LISTADO_JUEGOS_JSP = "listado-juegos.jsp";
+
 	public static final String CREAR_JUEGO_JSP = "crear-juego.jsp";
-	
-	
-	
+	public static final String INICIO = "inicio";
+	public static final String LISTADO_JUEGOS_JSP = "listado-juegos.jsp";
+
 	// mensajes alerta
-	
+
 	public static final String DANGER = "danger";
+	public static final String DELETE_CORRECTO = "Delete correcto";
+	public static final String DELETE_ERRONEO = "Delete erroneo";
 	public static final String INSERT_CORRECTO = "Insercion correcta";
+	public static final String INSERT_ERRONEO = "Insercion erronea";
 	public static final String NOMBRE_LONGITUD_INCORRECTA = "La longitud es incorrecta";
 	public static final String NOMBRE_YA_EXISTE = "El nombre ya existe";
 	public static final String SUCCESS = "success";
+	public static final String UPDATE_CORRECTO = "Actualizacion correcta";
+	public static final String UPDATE_ERRONEO = "Actualizacion erronea";
 	public static final String WARNING = "warning";
-	
-	
-	
-	//constantes numericas
-	
-	public static final int TRES=3;
-	public static final int CIEN=100;
+
+	// constantes numericas
+
+	public static final int TRES = 3;
+	public static final int CIEN = 100;
 
 }
