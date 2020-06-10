c292a9171bd94b9d0c96711dbe5f1bed7a6b9f7b
Alvaro <javaee@javaee-VirtualBox>
2020-06-10 13:52:54 +0200

    Eliminar registros

---
+++ catalogoJuegos/src/main/java/catalogoJuegos/controller/EliminarJuegoController.java
@@ -0,0 +1,64 @@
+package catalogoJuegos.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.bcel.classfile.Constant;
+
+import catalogoJuegos.modelo.Juego;
+import catalogoJuegos.modelo.JuegoDAOImpl;
+import catalogoJuegos.utilidades.Alerta;
+import catalogoJuegos.utilidades.Constantes;
+
+/**
+ * Servlet implementation class EliminarJuegoController
+ */
+@WebServlet(description = "Servlet para eliminar juegos", urlPatterns = { "/eliminar-juego" })
+public class EliminarJuegoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+		Juego juegoDelete=new Juego();
+		String id=request.getParameter("id");
+		juegoDelete.setId(Integer.parseInt(id));
+		Alerta alerta=new Alerta();
+		Juego juegoR=new Juego();
+		String vista="";
+		
+		JuegoDAOImpl dao=JuegoDAOImpl.getInstance();
+		try {
+			juegoR=new Juego();
+			juegoR=dao.delete(juegoDelete);
+			alerta.setMensaje(Constantes.DELETE_CORRECTO+": "+"["+juegoR.getId()+"] "+juegoR.getNombre());
+			alerta.setTipo(Constantes.SUCCESS);
+			vista=Constantes.INICIO;
+			
+		} catch (Exception e) {
+			alerta.setMensaje(Constantes.DELETE_ERRONEO);
+			alerta.setTipo(Constantes.DANGER);
+		}finally {
+			request.getSession().setAttribute("alerta", alerta);
+			request.setAttribute("juego", juegoR);
+			request.getRequestDispatcher(vista).forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
