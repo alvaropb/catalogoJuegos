f097b6166d825fcdddcd290ba2fd9f34a2dd50af
Alvaro <javaee@javaee-VirtualBox>
2020-06-12 13:21:32 +0200

    Eliminar import no utilizado

--- catalogoJuegos/src/main/java/catalogoJuegos/controller/EliminarJuegoController.java
+++ catalogoJuegos/src/main/java/catalogoJuegos/controller/EliminarJuegoController.java
@@ -1,13 +1,12 @@
 package catalogoJuegos.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.tomcat.util.bcel.classfile.Constant;
 
 import catalogoJuegos.modelo.Juego;
 import catalogoJuegos.modelo.JuegoDAOImpl;
