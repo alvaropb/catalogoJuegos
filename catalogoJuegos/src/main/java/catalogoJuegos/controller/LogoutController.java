cd00300dea54af1338dce557287b077dc46e2dc5
Alvaro <javaee@javaee-VirtualBox>
2020-06-12 13:20:46 +0200

    Logout de usuario

---
+++ catalogoJuegos/src/main/java/catalogoJuegos/controller/LogoutController.java
@@ -0,0 +1,47 @@
+package catalogoJuegos.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import catalogoJuegos.utilidades.Alerta;
+import catalogoJuegos.utilidades.Constantes;
+
+/**
+ * Servlet implementation class LogoutController
+ */
+@WebServlet(description = "Controller para logout", urlPatterns = { "/logout" })
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Alerta alerta=new Alerta();
+		
+		request.getSession().invalidate();
+		
+		alerta.setMensaje(Constantes.LOGOUT_CORRECTO);
+		alerta.setTipo(Constantes.SUCCESS);
+		
+		request.getSession().setAttribute("alerta", alerta);
+		request.getRequestDispatcher(Constantes.INICIO).forward(request, response);
+
+	}
+
+}
