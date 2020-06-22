8cc59b7d5408fe977c3414c0c5e6156555c65411
Alvaro <javaee@javaee-VirtualBox>
2020-06-22 13:54:56 +0200

    Se añade getAll LIMIT y getByCategoria

--- catalogoJuegos/src/main/java/catalogoJuegos/modelo/JuegoDAO.java
+++ catalogoJuegos/src/main/java/catalogoJuegos/modelo/JuegoDAO.java
@@ -1,5 +1,22 @@
 package catalogoJuegos.modelo;
 
-public interface JuegoDAO extends ICrudable<Juego> {
+import java.util.ArrayList;
 
+public interface JuegoDAO extends ICrudable<Juego> {
+	/**
+	 * Metodo que retorna los ultimos juegos
+	 * @param num
+	 * @return ArrayList<Juego> con los ultimos juegos
+	 * @throws Exception
+	 */
+	public ArrayList<Juego> getAll(int num) throws Exception;
+	
+	/**
+	 * Metodo que retorna un listado de juegos de una categoria
+	 * @param id
+	 * @param limite
+	 * @return ArrayList<Juego>
+	 * @throws Exception
+	 */
+	public ArrayList<Juego> getByCategoria(int id, int limite) throws Exception;
 }
