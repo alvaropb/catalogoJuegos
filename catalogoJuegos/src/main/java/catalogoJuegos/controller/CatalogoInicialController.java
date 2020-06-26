892bc0fe7a362eb041335350c8f8cb4f80da1ea6
Alvaro <javaee@javaee-VirtualBox>
2020-06-26 13:11:16 +0200

    Se añade carga inicial de juegos por categoria

--- catalogoJuegos/src/main/java/catalogoJuegos/controller/CatalogoInicialController.java
+++ catalogoJuegos/src/main/java/catalogoJuegos/controller/CatalogoInicialController.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
+
 import catalogoJuegos.modelo.impl.CategoriaDAOImpl;
 import catalogoJuegos.modelo.impl.JuegoDAOImpl;
 import catalogoJuegos.modelo.pojo.Categoria;
@@ -24,6 +27,8 @@ import catalogoJuegos.utilidades.Constantes;
 public class CatalogoInicialController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final JuegoDAOImpl juegoDAO=JuegoDAOImpl.getInstance();
+	private static final CategoriaDAOImpl categoriaDAO=CategoriaDAOImpl.getInstance();
+	private final static Logger LOG = Logger.getLogger(CatalogoInicialController.class);
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -47,7 +52,11 @@ public class CatalogoInicialController extends HttpServlet {
 
 	protected void doProccess(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		LOG.debug("prueba log4j");
+		
 		ArrayList<Juego> juegos = new ArrayList<Juego>();
+		ArrayList<Categoria>categorias=new ArrayList<Categoria>();
+		Categoria c=new Categoria();
 
 		Alerta alerta=new Alerta();
 		
@@ -58,20 +67,32 @@ public class CatalogoInicialController extends HttpServlet {
 		//redireccion
 		
 		RequestDispatcher dispatcher = null;
-		
+		dispatcher= request.getRequestDispatcher(Constantes.LISTADO_JUEGOS_INICIAL_JSP);
 		try {
+			
 			if (idCategoria==null || idCategoria.isEmpty()) {// si no viene informada la id, se muestran los ultimos 10 juegos
 				juegos = juegoDAO.getAll(Constantes.DIEZ);
-				dispatcher= request.getRequestDispatcher(Constantes.LISTADO_JUEGOS_INICIAL_JSP);
-			}else if("0".equals(idCategoria)) {
+				c.setJuegos(juegos);
+				c.setNombre("");
+				
+				categorias.add(c);
+				
+				
+			}else if("0".equals(idCategoria)) {// listado de juegos
 				juegos = juegoDAO.getAll();
 				dispatcher= request.getRequestDispatcher(Constantes.LISTADO_JUEGOS_JSP);
+				
+			}else if("-1".equals(idCategoria)) {// todos los juegos por categoria
+				categorias=categoriaDAO.getAllWithProducts();
 			}
-			
 			else {
 				int id=Integer.parseInt(idCategoria);
 				juegos=juegoDAO.getByCategoria(id,Constantes.DIEZ);
-				dispatcher= request.getRequestDispatcher(Constantes.LISTADO_JUEGOS_INICIAL_JSP);
+				c.setId(id);
+				c.setNombre(categoriaNombre);
+				c.setJuegos(juegos);
+				categorias.add(c);
+				
 			}
 
 		} catch (Exception e) {
@@ -82,7 +103,8 @@ public class CatalogoInicialController extends HttpServlet {
 		} finally {
 			
 			request.setAttribute(Constantes.CATEGORIA_NOMBRE, (categoriaNombre==null)?"Todas las categorias":"<b>"+juegos.size()+"</b> juego"+((juegos.size()==1)?"":"s")+" de la categoria <b>"+ categoriaNombre+"</b>" );
-			request.setAttribute(Constantes.JUEGOS, juegos);
+			request.setAttribute(Constantes.JUEGOS, juegos);// para el listado de juegos
+			request.setAttribute(Constantes.JUEGOS_POR_CATEGORIA, categorias);// para el listado de cards			
 			dispatcher.forward(request, response);
 			
 		}
