fe358c13ad52b125481dd0d5b69c5011a235d808
Alvaro <javaee@javaee-VirtualBox>
2020-06-26 13:12:29 +0200

    Se añade metodo getAllWithProducts

--- catalogoJuegos/src/main/java/catalogoJuegos/modelo/dao/CategoriaDAO.java
+++ catalogoJuegos/src/main/java/catalogoJuegos/modelo/dao/CategoriaDAO.java
@@ -1,8 +1,16 @@
 package catalogoJuegos.modelo.dao;
 
+import java.util.ArrayList;
+
 import catalogoJuegos.modelo.ICrudable;
 import catalogoJuegos.modelo.pojo.Categoria;
 
 public interface CategoriaDAO extends ICrudable<Categoria> {
 
+	/**
+	 * Metodo que devuelve un listado de juegos agrupados por categoria
+	 * @return ArrayList<Categoria>
+	 * @throws Exception
+	 */
+	public ArrayList<Categoria> getAllWithProducts()throws Exception ;
 }
