bb7c8305bf067ef82ca509e3c7804a8e3abe9e43
javaee <javaee@javaee-VirtualBox>
2020-07-17 13:55:04 +0200

    Se añade filtro y controlador para backoffice

---
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/controller/BackofficeController.java
@@ -0,0 +1,46 @@
+package com.catalogoJuegos.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.catalogoJuegos.utilidades.Constantes;
+
+/**
+ * Servlet implementation class BackofficeController
+ */
+@WebServlet("/views/backoffice/inicio")
+public class BackofficeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOG=Logger.getLogger(BackofficeController.class);
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LOG.trace("Entra en BackofficeController doGet() request:"+request);
+		// cargar en atributos los datos que recogemos del DAO
+		
+		request.setAttribute("datos1", "listado de datos 1");
+		request.setAttribute("datos2", "listado de datos 2");
+		
+		// redireccion 
+		
+		request.getRequestDispatcher(Constantes.INDEX).forward(request, response);
+		LOG.trace("Entra en BackofficeController doGet() response:"+response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/filter/BackofficeFilter.java
@@ -0,0 +1,84 @@
+package com.catalogoJuegos.filter;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.catalogoJuegos.modelo.pojo.Rol;
+import com.catalogoJuegos.modelo.pojo.Usuario;
+
+
+
+/**
+ * Servlet Filter implementation class BackofficeFilter
+ */
+@WebFilter(dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD, 
+				DispatcherType.INCLUDE, 
+				DispatcherType.ERROR
+		}
+					, description = "Filtro para la url de backoffice", urlPatterns = { "/views/backoffice/*" })
+public class BackofficeFilter implements Filter {
+	private static final Logger LOG=Logger.getLogger(BackofficeFilter.class);
+
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		LOG.trace("Destroy del filtro");
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// parsear servler request a httprequest
+		HttpServletRequest servletRequest=(HttpServletRequest)request;
+		//parsear servler response a httpresponse
+		HttpServletResponse servletResponse=(HttpServletResponse)response;
+		
+		// sacar el usuario de la sesion
+		Usuario usu=(Usuario) servletRequest.getSession().getAttribute("usuario");
+		
+		// sacar el  contexto de la aplicacion 
+		String contextPath=servletRequest.getContextPath();
+		
+		
+		if (usu==null) {// si usuario es NULL , redirigir a login
+			servletResponse.sendRedirect(contextPath+"/login");
+			//servletRequest.getRequestDispatcher(contextPath+"/views/login/login.jsp").forward(servletRequest, servletResponse);//TODO mirar si hace falta poner el contextPath
+			LOG.trace("intento de entrar a :"+servletRequest.getPathInfo()+" desde:"+servletRequest.getLocalAddr());
+		}else if(Rol.ADMINISTRADOR==usu.getRol().getId()) {// si usuario es admin->backoffice
+			// pass the request along the filter chain
+		chain.doFilter(request, response);
+		LOG.debug("redireccion correcta");
+			
+		}else {// si usuario es normal-->frontoffice
+			LOG.trace("intento de entrar a :"+servletRequest.getPathInfo()+" desde:"+servletRequest.getLocalAddr());
+			servletResponse.sendRedirect(contextPath+"/views/frontoffice/index.jsp");
+		}
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		LOG.trace("Inicio del filtro");
+
+	}
+
+}
