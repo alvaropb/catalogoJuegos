8a495d09feb629ae7a84ff32d628ccb10af2b974
javaee <javaee@javaee-VirtualBox>
2020-07-17 13:54:20 +0200

    Se añade rol

---
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/pojo/Rol.java
@@ -0,0 +1,37 @@
+package com.catalogoJuegos.modelo.pojo;
+
+public class Rol {
+	public static final int ADMINISTRADOR=2;
+	public static final int USUARIO=1;
+	private int id;
+	private String nombre;
+	
+	public Rol() {
+		super();
+		this.id=0;
+		this.nombre="";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Rol [id=" + id + ", nombre=" + nombre + "]";
+	}
+
+	
+}
