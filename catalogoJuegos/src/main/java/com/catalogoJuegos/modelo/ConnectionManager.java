7969814dfede49e0eb9b558a748080ad8076eebf
javaee <javaee@javaee-VirtualBox>
2020-07-17 14:01:22 +0200

    Se añade pool de conexiones

--- catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/ConnectionManager.java
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/ConnectionManager.java
@@ -1,8 +1,13 @@
 package com.catalogoJuegos.modelo;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class ConnectionManager {
 
@@ -12,16 +17,24 @@ public class ConnectionManager {
 	private final static String USUARIO = "debian-sys-maint";
 	private final static String PASS = "o8lAkaNtX91xMUcV";
 	
+ 
 	
-	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException, NamingException {
 		
-		Connection con = null;
+		InitialContext initCtx=new InitialContext();;
+		Context envCtx = (Context) initCtx.lookup("java:comp/env");
+		DataSource dataSource = (DataSource)envCtx.lookup("jdbc/poolConnection");
+		Connection con = dataSource.getConnection();
+//		Connection connection=dataSource.getConnection();
 		
-		// comprobar que tengamos el .jar de MySQL
-		Class.forName("com.mysql.jdbc.Driver");
-		
-		//establecer conexion
-		con = DriverManager.getConnection(URL, USUARIO, PASS);
+//		Connection con = null;
+//		
+//		// comprobar que tengamos el .jar de MySQL
+//		Class.forName("com.mysql.jdbc.Driver");
+//		
+//		//establecer conexion
+//		con = DriverManager.getConnection(URL, USUARIO, PASS);
 		
 		return con;
 	};
