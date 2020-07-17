a9fd53fa427121e1436e4335c1b7f00a8daa8051
javaee <javaee@javaee-VirtualBox>
2020-07-17 14:04:48 +0200

    Se añaden ctes categorias, frontoffice, backoffice

--- catalogoJuegos/src/main/java/com/catalogoJuegos/utilidades/Constantes.java
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/utilidades/Constantes.java
@@ -5,28 +5,44 @@ public class Constantes {
 	// atributos
 
 	public static final String ALERTA = "alerta";
+	public static final String CATEGORIA = "categoria";
 	public static final String CATEGORIAS = "categorias";
 	public static final String CATEGORIA_NOMBRE = "categoriaNombre";
 	public static final String ID_CATEGORIA = "idCategoria";
 	public static final String INICIO_JUEGOS = "Inicio | juegos";
 	public static final String JUEGOS = "juegos";
 	public static final String JUEGOS_POR_CATEGORIA = "juegosCategoria";
+	public static final String MSJ = "msj";
+	public static final String NOMBRE = "nombre";
+	public static final String OPERACION = "operacion";
 	public static final String PAGINA = "pagina";
+
+	public static final String VALIDAR = "validar";
 
 	// paginas
 
+	public static final String BACKOFFICE = "views/backoffice/index.jsp";
+	public static final String BACKOFFICE_CONTROLLER = "views/backoffice/inicio";
+	public static final String CREAR_CATEGORIA = "views/categorias/crear-categoria.jsp";
 	public static final String CREAR_JUEGO_JSP = "views/juegos/crear-juego.jsp";
+	public static final String FORMULARIO = "formulario.jsp";
+	public static final String FRONTOFFICE = "views/frontoffice/index.jsp";
+	public static final String FRONTOFFICE_CONTROLLER = "views/frontoffice/inicio";
+	public static final String INDEX = "index.jsp";
 	public static final String INICIO = "inicio";
+	public static final String JUEGOS_JSP = "juegos.jsp";
+	public static final String LISTADO_CATEGORIAS_JSP = "views/categorias/listado-categorias.jsp";
 	public static final String LISTADO_JUEGOS_INICIAL_JSP = "views/juegos/cards-inicio.jsp";
 	public static final String LISTADO_JUEGOS_JSP = "views/juegos/listado-juegos.jsp";
 	public static final String LOGIN = "views/login/login.jsp";
-
+	
 	// mensajes alerta
 
 	public static final String DANGER = "danger";
 	public static final String DELETE_CORRECTO = "Delete correcto";
 	public static final String DELETE_ERRONEO = "Delete erroneo";
 	public static final String ERROR_INESPERADO = "Ocurrio un error inesperado";
+	public static final String ERROR_INTEGRIDAD = "No ha sido posible eliminar la categoria ya que tiene juegos asociados";
 	public static final String INSERT_CORRECTO = "Insercion correcta";
 	public static final String INSERT_ERRONEO = "Insercion erronea";
 	public static final String LOGIN_CORRECTO = "Login correcto";
@@ -40,7 +56,9 @@ public class Constantes {
 	public static final String WARNING = "warning";
 
 	// mensajes h1
+	public static final String MSJ_CREAR_CATEGORIA = "Crear categoria";
 	public static final String MSJ_CREAR_JUEGO = "Crear juego";
+	public static final String MSJ_EDITAR_CATEGORIA = "Editar categoria";
 	public static final String MSJ_EDITAR_REGISTRO = "Editar juego";
 	public static final String MSJ_VER_DETALLE = "Detalle";
 
@@ -49,5 +67,20 @@ public class Constantes {
 	public static final int TRES = 3;
 	public static final int DIEZ = 10;
 	public static final int CIEN = 100;
+	
+	// errores
+	public static final String DUPLICATE_ENTRY = "Duplicate entry";
+	public static final String INTEGRITY_ERROR = "Cannot delete or update a parent row: a foreign key constraint fails";
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
