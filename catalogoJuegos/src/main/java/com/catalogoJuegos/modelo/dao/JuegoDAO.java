6bcc6a1d5ee047e9c650e17093aa73277ed9a3b5
Alvaro <alvaro@test.com>
2020-07-22 12:05:15 +0200

    getAllByUser,getResumenByIdUsuario,delete con permisos,getById con permisos

--- catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/dao/JuegoDAO.java
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/dao/JuegoDAO.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.catalogoJuegos.modelo.ICrudable;
 import com.catalogoJuegos.modelo.pojo.Juego;
+import com.catalogoJuegos.modelo.pojo.Resumen;
 
 public interface JuegoDAO extends ICrudable<Juego> {
 	/**
@@ -22,4 +23,38 @@ public interface JuegoDAO extends ICrudable<Juego> {
 	 * @throws Exception
 	 */
 	public ArrayList<Juego> getByCategoria(int id, int limite) throws Exception;
+	
+	/**
+	 * Metodo que retorna un listado de juegos de un usuario
+	 * @param id el id del usuario para buscar sus juegos
+	 * @param isValidado buscara juegos validados o no
+	 * @return ArrayList<Juego>
+	 * @throws Exception
+	 */
+	public ArrayList<Juego> getAllByUser(int id,boolean isValidado)throws Exception;
+	
+	/**
+	 * Metodo que retorna un resumen con los juegos totales, validados y pdtes de validar
+	 * @param id
+	 * @return Resumen
+	 * @throws Exception
+	 */
+	public Resumen getResumenById(int id)throws Exception;
+	/**
+	 * Metodo que elimina un registro siempre y cuando se tengan permisos
+	 * @param idJuego
+	 * @param idUsuario
+	 * @return
+	 * @throws SeguridadException
+	 */
+	public Juego delete(int idJuego, int idUsuario)throws Exception;
+	
+	/**
+	 * Metodo que retorna un juego por su id_usuario
+	 * @param idProducto
+	 * @param idUsuario
+	 * @return Juego
+	 * @throws SeguridadException
+	 */
+	public Juego getById(int idJuego, int idUsuario) throws SeguridadException;
 }
