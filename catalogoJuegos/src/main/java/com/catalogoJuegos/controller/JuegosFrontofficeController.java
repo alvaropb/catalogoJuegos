b23092524609a249348bd9021ba534a3d3591dc8
Alvaro <alvaro@test.com>
2020-07-22 12:03:03 +0200

    TODO redireccion correcta en funcion validacion juego

--- catalogoJuegos/src/main/java/com/catalogoJuegos/controller/JuegosFrontofficeController.java
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/controller/JuegosFrontofficeController.java
@@ -1,50 +1,77 @@
 package com.catalogoJuegos.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
+import com.catalogoJuegos.modelo.impl.JuegoDAOImpl;
+import com.catalogoJuegos.modelo.pojo.Juego;
+import com.catalogoJuegos.modelo.pojo.Usuario;
 import com.catalogoJuegos.utilidades.Constantes;
 
 /**
  * Servlet implementation class ProductosFrontofficeController
  */
-@WebServlet("/views/frontoffice/productos")
+@WebServlet("/views/frontoffice/juegos")
 public class JuegosFrontofficeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private static final Logger LOG = Logger.getLogger(JuegosFrontofficeController.class);
+	private static final JuegoDAOImpl juegoDAO = JuegoDAOImpl.getInstance();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//recoger parametros
-		String validados=request.getParameter(Constantes.VALIDAR);
-		
-		// buscar productos validados o sin validar
-		if (validados==null) {	
-			//productos sin validar
-			request.setAttribute(Constantes.JUEGOS, "Juegos sin validar");
-		}else {
-			//productos validados
-			request.setAttribute(Constantes.JUEGOS, "Juegos validados");
-			
-		}
-		//redirigir a productos.jsp
-		request.getRequestDispatcher(Constantes.JUEGOS_JSP).forward(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ArrayList<Juego> juegos = new ArrayList<Juego>();
 
-		
-		
+		// recoger parametros
+		String validados = request.getParameter(Constantes.VALIDAR);
+		//TODO recoger de CrearJuegosController un parametro, atributo o lo que sea para redirigir 
+		//en funcion de si el juego editado esta validado o noString validadosDeController=request.getParameter(Constantes.VALIDAR);
+		LOG.trace("validados: "+validados);
+		Usuario usu = (Usuario) request.getSession().getAttribute(Constantes.USUARIO);
+		LOG.trace("usuario: "+usu);
+
+		try {
+			// buscar productos validados o sin validar
+			if (validados == null) {
+				// productos sin validar
+				juegos = juegoDAO.getAllByUser(usu.getId(), false);
+
+			} else {
+				// productos validados
+				juegos = juegoDAO.getAllByUser(usu.getId(), true);
+
+			}
+
+		} catch (Exception e) {
+			LOG.error(e);
+		} finally {
+			request.setAttribute(Constantes.JUEGOS, juegos);
+		}
+		// redirigir a juegos.jsp
+		request.getRequestDispatcher(Constantes.JUEGOS_JSP).forward(request, response);
+	
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 		doGet(request, response);
 	}
 
