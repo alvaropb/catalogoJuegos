ca0e6adcbdc7860a766042043076cbd50908ac80
Alvaro <javaee@javaee-VirtualBox>
2020-07-22 11:51:58 +0200

    Se comenta la clase

--- catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/dao/SeguridadException.java
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/dao/SeguridadException.java
@@ -3,7 +3,7 @@ package com.catalogoJuegos.modelo.dao;
 public class SeguridadException extends Exception{
 
 	/**
-	 * 
+	 * Clase que se lanza en caso de que se intente saltar la seguridad al actualizar sin permisos necesarios
 	 */
 	private static final long serialVersionUID = 1L;
 	public static final String MENSAJE="No tiene permisos para efectuar esta operacion";
