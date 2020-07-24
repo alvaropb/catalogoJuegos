06e994e061487062479cf84fbef0bbac616eda0c
Alvaro <alvaro@test.com>
2020-07-24 13:20:01 +0200

    Se quita codigo que no se usa

--- catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/ConnectionManager.java
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/ConnectionManager.java
@@ -12,29 +12,13 @@ import javax.sql.DataSource;
 public class ConnectionManager {
 
 	
-	
-	private final static String URL = "jdbc:mysql://localhost/catalogoJuegos";
-	private final static String USUARIO = "debian-sys-maint";
-	private final static String PASS = "o8lAkaNtX91xMUcV";
-	
- 
-	
-	
 	public static Connection getConnection() throws SQLException, ClassNotFoundException, NamingException {
 		
 		InitialContext initCtx=new InitialContext();;
 		Context envCtx = (Context) initCtx.lookup("java:comp/env");
 		DataSource dataSource = (DataSource)envCtx.lookup("jdbc/poolConnection");
 		Connection con = dataSource.getConnection();
-//		Connection connection=dataSource.getConnection();
-		
-//		Connection con = null;
-//		
-//		// comprobar que tengamos el .jar de MySQL
-//		Class.forName("com.mysql.jdbc.Driver");
-//		
-//		//establecer conexion
-//		con = DriverManager.getConnection(URL, USUARIO, PASS);
+
 		
 		return con;
 	};
