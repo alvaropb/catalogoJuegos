e99c0a42ef97a8bbaad06278867da53682c322ce
Alvaro <alvaro@test.com>
2020-10-05 13:55:52 +0200

    Documentacion

--- catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/dao/JuegoDAO.java
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/modelo/dao/JuegoDAO.java
@@ -9,13 +9,13 @@ import com.catalogoJuegos.modelo.pojo.Usuario;
 
 /**
  * Interfaz para agregar metodos no contemplados en ICrudable para los Juegos
- * @author javaee
- *
+ * @author Alvaro PB
+ * @version 1.0 
  */
 public interface JuegoDAO extends ICrudable<Juego> {
 	/**
 	 * Metodo que retorna los ultimos juegos
-	 * @param num
+	 * @param num numero de juegos a listar
 	 * @return ArrayList<Juego> con los ultimos juegos
 	 * @throws Exception
 	 */
@@ -31,8 +31,8 @@ public interface JuegoDAO extends ICrudable<Juego> {
 	
 	/**
 	 * Metodo que retorna un listado de juegos de una categoria
-	 * @param id
-	 * @param limite
+	 * @param id el id de la categoria
+	 * @param limite limite de resultados maximos
 	 * @return ArrayList<Juego>
 	 * @throws Exception
 	 */
@@ -49,26 +49,26 @@ public interface JuegoDAO extends ICrudable<Juego> {
 	
 	/**
 	 * Metodo que retorna un resumen con los juegos totales, validados y pdtes de validar
-	 * @param id
+	 * @param id el id de usuario
 	 * @return Resumen
 	 * @throws Exception
 	 */
 	public Resumen getResumenById(int id)throws Exception;
 	/**
 	 * Metodo que elimina un registro siempre y cuando se tengan permisos
-	 * @param idJuego
-	 * @param idUsuario
-	 * @return
+	 * @param idJuego el id del juego
+	 * @param idUsuario el id del usuario
+	 * @return Resumen dcon datos obtenidos de una vista
 	 * @throws SeguridadException
 	 */
 	public Juego delete(int idJuego, int idUsuario)throws Exception;
 	
 	/**
 	 * Metodo que retorna un juego por su id_usuario
-	 * @param idProducto
-	 * @param idUsuario
-	 * @return Juego
-	 * @throws SeguridadException
+	 * @param idJuego el id del juego
+	 * @param idUsuario el id del usuario
+	 * @return Juego el juego buscado
+	 * @throws SeguridadException se lanza si no tiene permisos para efectuar esta operacion
 	 * @throws Exception 
 	 */
 	public Juego getById(int idJuego, int idUsuario) throws SeguridadException, Exception;
@@ -85,7 +85,7 @@ public interface JuegoDAO extends ICrudable<Juego> {
 	 * @param Usuario u usuario conectado
 	 * @return El juego actualizado
 	 * @throws Exception
-	 * @throws SeguridadException
+	 * @throws SeguridadException se lanza si no tiene permisos para efectuar esta operacion
 	 */
 	public Juego update(Juego t, Usuario u) throws Exception,SeguridadException;
 }
