04418d762d1df5bed07171f67051b7af12bc781c
Alvaro <alvaro@test.com>
2020-10-05 13:55:17 +0200

    Documentacion del paquete controller

---
+++ catalogoJuegos/src/main/java/com/catalogoJuegos/controller/package-info.java
@@ -0,0 +1,8 @@
+package com.catalogoJuegos.controller;
+
+/**
+ * Paquete con los controladores de catalogoJuegos
+ * tanto de administrador como de usuario.
+ * @version 1.0 
+ * @author Alvaro PB
+ */
